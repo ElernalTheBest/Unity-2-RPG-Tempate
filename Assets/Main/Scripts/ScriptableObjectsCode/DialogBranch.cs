ae9f04029e6b5ff2043446d85a0446cb7694d89e
ElernalTheGreat <dcw.zheng@gmail.com>
2020-07-08 17:12:35 -0700

    Completed Dialog
    yeet

---
+++ Assets/Main/Scripts/ScriptableObjectsCode/DialogBranch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Branch"
+, menuName="CustomObject/Dialogue/Branch")]
+public class DialogBranch : ScriptableObject
+{
+    public string dialogueID = "";
+    public List<string> DialogueLines;
+    public List<ResponseBranch> ResponseOption;
+}
+[System.Serializable]
+
+public class ResponseBranch
+
+{
+        public string text;
+        public DialogBranch nextBranch;
+
+}
